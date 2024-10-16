37cd24aaa3cbd008bfd8093f3f8c6cd48e94c4db
JhonBG <jhon.botero1@udea.edu.co>
2024-10-16 09:22:30 -0500

    Corrección de errores

--- src/components/molecules/ExtraSkill/index.tsx
+++ src/components/molecules/ExtraSkill/index.tsx
@@ -4,7 +4,7 @@ import { TextPrincipalTertiary } from '@/components/atoms/titles'
 const Index = ({texto}:{texto:string}) => {
   return (
     <div className='flex flex-row gap-2 items-center justify-start'>
-      <img src='/logos/Icons.svg'/>
+      <img src='/logos/Icons.svg' alt='No se encontro imagen'/>
       <TextPrincipalTertiary text={texto}/>
     </div>
   )
